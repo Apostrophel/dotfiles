2a1678229c5d4e29a849cce208fce222a7a74aa5
Sjur Barndon <sjurbarndon@proton.me>
2025-08-31 15:45:30 +0200

    added some different configs

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -26,4 +26,14 @@ return {
   -- 		},
   -- 	},
   -- },
+
+  {
+    "lervag/vimtex",
+    lazy = false,
+    init = function()
+      vim.g.vimtex_view_method = "zathura"
+      vim.g.vimtex_compiler_method = "latexmk"
+      vim.opt.conceallevel = 0
+    end,
+  },
 }
